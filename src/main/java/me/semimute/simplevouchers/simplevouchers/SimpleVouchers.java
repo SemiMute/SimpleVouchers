f594c625f37fd96aac9cfc41ce412a752de3fee2
SemiMute <44712640+SemiMute@users.noreply.github.com>
2022-11-25 17:49:25 -0800

    base plugin

---
+++ src/main/java/me/semimute/simplevouchers/simplevouchers/SimpleVouchers.java
@@ -0,0 +1,17 @@
+package me.semimute.simplevouchers.simplevouchers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimpleVouchers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
